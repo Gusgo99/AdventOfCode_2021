897d54b0f7cb33b2886af0f608595e5a9e5c0e2e
Gustavo Gonçalves <gustavo@crazy4options.com>
2021-12-01 23:19:11 -0300

    Added template for project.

---
+++ include/headers.hpp
@@ -0,0 +1,22 @@
+#ifndef HEADERS_HPP
+#define HEADERS_HPP
+
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <tuple>
+#include <string>
+#include <stack>
+#include <chrono>
+#include <fstream>
+#include <iterator>
+#include <iostream>
+#include <variant>
+#include <optional>
+#include <regex>
+
+#endif
